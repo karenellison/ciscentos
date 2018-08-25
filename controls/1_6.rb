7b77b7d47d65d2a76ef4a1e125e7ec5e5916b8bf
Masliak Kateryna <masliak.kateryna@gmail.com>
2018-08-25 23:22:46 +0200

    Added 1.6.1.5 control

--- controls/1_6.rb
+++ controls/1_6.rb
@@ -6,3 +6,11 @@ control "cis-1-6-1-4" do
         it { should_not be_installed }
     end
 end
+control "cis-1-6-1-5" do
+    impact 1.0
+    title "1.6.1.5 Ensure the MCS Translation Service (mcstrans) is not installed (Scored)"
+    desc "The mcstransd daemon provides category label information to client processes requesting information. The label translations are defined in /etc/selinux/targeted/setrans.conf"
+    describe package('mcstrans') do
+        it { should_not be_installed }
+    end
+end
